ba2d57fdbaaaab4153d56ca98f3afb48247d6d5f
Tomas Maiza <tmaiza@uc.cl>
2023-01-12 23:04:21 -0300

    Basic controllers and their methods created. There are some questions written in routes.rb about nested routes and needs to be resolved next time

---
+++ app/controllers/animal_types_controller.rb
@@ -0,0 +1,23 @@
+class AnimalTypesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @animal_types = AnimalType.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :owners
-  devise_for :vets, path: 'vets'
+  devise_for :owners, path: 'owners', controllers: {
+    sessions: 'owners/sessions'
+  }
+  devise_for :vets, path: 'vets', controllers: {
+    sessions: 'vets/sessions'
+  }
   # eg. http://localhost:3000/vets/sign_in
   devise_for :users, path: 'users'
   root to: "pages#home"
@@ -8,4 +12,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'veterinaries/index', to: 'veterinaries#index'
+  get 'record_types/index', to: 'record_types#index'
+  get 'animal_types/index', to: 'animal_types#index'
+  resources :pets do
+    resources :records
+  end
 end
+#Preguntas para siguiente clase: ¿Será necesario nestear rutas de vets en veterinaries?
+#¿Y nestear pets dentro de owners? Yo creo que sí.
+#¿Nestear records dentro de record_types?
