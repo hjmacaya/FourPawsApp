a224a87b1acdb082e3cfac0307e02237d7478f0b
Tomas Maiza <tmaiza@uc.cl>
2023-01-17 22:41:21 -0300

    Added a new record form

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,20 +1,26 @@
 class RecordsController < ApplicationController
   skip_before_action :authenticate_user!
   def new
+    @pet = Pet.find(params[:pet_id])
     @record = Record.new
   end
 
   def create
     @record = Record.new(record_params)
+    @pet = Pet.find(params[:pet_id])
+    @record.vet = current_vet
+    @record.pet = @pet
     if @record.save
-      redirect_to @record
+      redirect_to record_path(@record)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def index
-    @records = Record.all
+    records = Record.all
+    @current_veterinary = current_vet.veterinary
+    @records = records.select { |record| record.vet.veterinary == @current_veterinary }
   end
 
   def show
@@ -43,6 +49,6 @@ class RecordsController < ApplicationController
   private
 
   def record_params
-    params.require(:record).permit(:name, :observation, :date, :record_type)
+    params.require(:record).permit(:name, :observation, :date, :record_type_id)
   end
 end
