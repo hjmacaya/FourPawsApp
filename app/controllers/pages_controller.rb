9e2da2553331a5bb0e3f1eea35812848dc244a11
Hernan Macaya <hjmacaya@uc.cl>
2023-01-19 12:37:15 -0300

    login functional n' almost ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: %i[home login_view]
 
   def home
   end
 
   def vet_home
+  end
+
+  def login_view
   end
 end
