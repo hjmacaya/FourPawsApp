9d9ed02f13035f5fdaf2d4524977f335878b14e4
Hernan Macaya <hjmacaya@uc.cl>
2023-01-24 13:21:54 -0300

    AS and cloudinary are working

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,8 @@ class Pet < ApplicationRecord
   belongs_to :owner
   belongs_to :animal_type
   has_many :records
+  # Ataching a img
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "only letters allowed" }
